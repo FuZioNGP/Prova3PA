9b0fa343ca1b8c926b8be4adb230f8a7c734cb28
JL-conrado <joaolucasc@ucl.br>
2022-06-12 13:20:37 -0400

    Funcao leitura da cadeia e adicionado comentarios

--- MinhaBiblioteca.c
+++ MinhaBiblioteca.c
@@ -2,8 +2,9 @@
 
 void max_min(int nums[], int *max, int *min, int nElementos)
 {
+  //cadeia de 2 números
   if (nums[0] > nums[1])
-  {
+  { 
     *max = nums[0];
     *min = nums[1];
   }
@@ -12,14 +13,12 @@ void max_min(int nums[], int *max, int *min, int nElementos)
     *max = nums[1];
     *min = nums[0];
   }
-  
+  //cadeia de mais que dois numeros
   for(int i = 2; i < nElementos; i++)
   {
     if (nums[i] > *max){
       *max = nums[i];
     } 
-      
-         
     if (nums[i] < *min) {
       *min = nums[i];
     } 
--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main(void)
   printf("Insira a quantidade de entradas da cadeia desejada! \n");
   scanf("%d", &nElementos);
 
-   int nums[nElementos];
-
-
+  //vetor
+  int nums[nElementos];
+  //cadeia maior que 2 ou igual a 2
   if(nElementos >= 2)
   {
     for(int i = 0; i < nElementos; i++)
@@ -20,6 +20,7 @@ int main(void)
       scanf("%d", &nums[i]);
     }
   }
+  //cadeia de apenas um numero entra nesta função
   else if(nElementos == 1)
   {
     int num;
@@ -27,13 +28,31 @@ int main(void)
     printf("O numero escolhido foi: %d\n", num);
     printf("Max=%d, Min=%d\n",num,num);
     return 0;
-  }
-  else
+  } 
+  else //caso insira um numero invalido por exemplo 0 ou -1 cai neste erro.
   {
     printf("Quantidade Invalida!\n");
     return 0;
   }
+
+  printf("Os numeros inseridos foram: \n");
+  //função que separa a cadeia por virgulas
+  for (int i = 0; i < nElementos; i++) 
+  {
+    printf("%d", nums[i]);
+    if (i == (nElementos-1))
+    {
+      printf("\n");
+    } 
+    else 
+    {
+      printf(",");
+    }
+  }
+  //função void sem retorno.
   max_min(nums, &max, &min, nElementos);
+  //resultado da função
   printf("Max=%d, Min=%d", max, min);
+
   return 0;
 }
